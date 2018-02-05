c399496a2824c265a9d269a7d530f772cebeba6f
airetriton <wobbe_espinoza@hotmail.com>
2018-02-04 21:57:50 -0700

    five

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
+mongoose.connect("mongodb://localhost/huffscrapedb", {
   useMongoClient: true
 });
 
@@ -38,12 +38,13 @@ mongoose.connect("mongodb://localhost/week18Populater", {
 // A GET route for scraping the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.huffingtonpost.com/topic/activism").then(function(response) {
+
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
     
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h2").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
@@ -54,6 +55,9 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
+      // result.link = $(this)
+      //   .children("class",".card_image_src")
+      //   .attr("background-image", "url")  
 
       // Create a new Article using the `result` object built from scraping
       db.Article
